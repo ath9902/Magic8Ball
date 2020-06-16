2dec7e03931644cb5cc5c8c217c740e1aff398fb
Austin Heisey <ath9902@rit.edu>
2020-06-16 18:42:04 -0400

    moved project

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = [#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    override func viewDidLoad() {
+        imageView.image = ballArray[1]
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?){
+        if motion == .motionShake {
+             imageView.image = ballArray.randomElement()
+        }
+    }
+    
+
+}
+
